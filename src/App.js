9c2129c84a89668ea4524624e32ecdbe493fc0fa
mangesh <mshanit@gmail.com>
2020-03-03 01:49:01 +0530

    React Project For Showing Characters Of A string

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import List from './show/listShow';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component{
+  state={
+    str:'hello my man',
+    showlist:false,
+    showstr:true
+  }
+  changeHandler=(event)=>{
+    this.setState({
+      str:event.target.value,
+    })
+  }
+  listHandler=()=>{
+    this.setState({
+      showlist:!this.state.showlist
+    })
+  }
+  deleteHandler=(index)=>{
+    const  str=[...this.state.str];
+    str.splice(index,1);
+    this.setState({str:str})
+  }
+  
+
+  render(){
+
+    let str=[...this.state.str];
+
+    
+    let characters=null;
+    if(this.state.showlist){
+        characters=(
+            <div>
+              
+            {str.map((single,index)=>{
+               return( 
+                 <List item={single} onClick={()=>this.deleteHandler(index)}/>
+                )
+            })}
+        </div>
+        )
+    }
+  
+    
+
+  
+    return(
+      <div>
+        <h1>Hello React</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {str=[...this.state.str]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <input type='text' onChange={this.changeHandler} value={this.state.str}></input>
+        <p>Entered Text={this.state.str}</p>
+        <button onClick={this.listHandler}>Show list</button>
+        <button>Show string</button><br/>
+        {characters}
+      </div>
+    );
+  }
 }
 
 export default App;
